5917bc619227d8a62aa87a3ff000636cbb16e222
Carton Liu <1435742009@qq.com>
2025-09-29 20:45:30 -0500

    Initial website

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", padding: "2rem" }}>
+      <h1>CHI 2026 Smart Glass Workshop</h1>
+      <p>Welcome to the official workshop website!</p>
+      <p>📅 Coming in 2026</p>
+      <p>📍 Location: ACM CHI 2026 Conference</p>
+      <footer style={{ marginTop: "2rem", fontSize: "0.8rem", color: "gray" }}>
+        © 2026 Smart Glass Workshop
+      </footer>
+    </div>
+  );
+}
+
+export default App;
